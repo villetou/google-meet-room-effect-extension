e6b4b4fda417856a50b149b7e7e810cad1e0928a
Ville Touronen <touronen@gmail.com>
2020-11-25 00:16:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# A Google Meet extension for a background chatter effect
+
+This is an initial test for the extension. Don't blame me if it doesn't work.
---
+++ src/index.js
@@ -0,0 +1,125 @@
+async function loadImpulseBuffer(audioCtx) {
+    // load impulse response from file
+
+    const pathToImpulse = 'impulses/252846__kijjaz__20141025-ir-new-office-02.wav'
+    let url = ''
+
+    if (typeof browser !== 'undefined') {
+        url = browser.runtime.getURL(pathToImpulse)
+    } else if (typeof chrome !== 'undefined') {
+        url = chrome.runtime.getURL(pathToImpulse)
+    }
+
+    let response = await fetch(url)
+    let arraybuffer = await response.arrayBuffer()
+    return await audioCtx.decodeAudioData(arraybuffer)
+}
+
+async function createAudioChain(ctx) {
+    // Create dry signal chain
+    const dry = ctx.createGain()
+    dry.connect(ctx.destination)
+
+
+    // Create parallel chain for distant chatter effect
+    // Src -> Lowpass filter -> Reverb (Convolver) -> Output 
+
+    const wetInput = ctx.createGain()
+
+    wetInput.gain.value = 1
+
+    const panRight = ctx.createStereoPanner()
+    panRight.pan.value = 0.8
+
+    const delayRight = ctx.createDelay()
+    delayRight.delayTime.value = 0.015
+
+    const panLeft = ctx.createStereoPanner()
+    panLeft.pan.value = -0.8
+
+    const filter = ctx.createBiquadFilter()
+    filter.type = 'bandpass'
+    filter.frequency.value = 1400
+    filter.Q.value = 4
+
+    const reverb = ctx.createConvolver()
+    reverb.buffer = await loadImpulseBuffer(ctx)
+
+    const wet = ctx.createGain()
+
+    // -- Make connections --
+    // Split wet input to panning nodes, merge panned signals to filter
+    wetInput.connect(panRight).connect(delayRight).connect(filter)
+    wetInput.connect(panLeft).connect(filter)
+
+    // Connect rest of the effect chain to output
+    filter.connect(reverb).connect(wet).connect(ctx.destination)
+
+    return {
+        connect: (srcNode) => {
+            srcNode.connect(dry)
+            srcNode.connect(wetInput)
+        },
+        toBack: (time) => {
+            console.log(time)
+            wet.gain.linearRampToValueAtTime(1.0, ctx.currentTime + time)
+            dry.gain.linearRampToValueAtTime(0.0, ctx.currentTime + time)
+        },
+        toFront: (time) => {
+            console.log(time)
+            wet.gain.linearRampToValueAtTime(0.0, ctx.currentTime + time)
+            dry.gain.linearRampToValueAtTime(1.0, ctx.currentTime + time)
+        },
+        filter: filter
+    }
+}
+
+async function initializeExtension() {
+
+    if (!document.querySelector('[aria-label="Leave call"]')) return null;
+
+    console.log('Loading background chatter extension!')
+
+    const ctx = new AudioContext()
+
+    const audioChain = await createAudioChain(ctx)
+
+    const audioElements = document.getElementsByTagName('audio')
+
+    Array.from(audioElements).map(audioElement => {
+        node = ctx.createMediaStreamSource(audioElement.srcObject)
+        audioChain.connect(node)
+        audioElement.muted = true
+        audioElement.volume = 0
+        audioElement.pause()
+        console.log('Detected audio element!')
+    })
+
+    audioChain.toFront(1.0)
+
+    function handleVisibilityChange() {
+
+        if (document.visibilityState === 'hidden') {
+            // Tab unfocused, take audio to back
+            console.log("Out of focus")
+            audioChain.toBack(1.0)
+        } else {
+            // Tab focused, bring audio to front
+            console.log("Tab focused")
+            audioChain.toFront(1.0)
+        }
+    }
+
+    document.addEventListener('visibilitychange', handleVisibilityChange, false)
+
+    return audioChain
+}
+
+async function initLoop() {
+    const audioChain = await initializeExtension()
+    if (!audioChain) {
+        setTimeout(initLoop, 1000)
+    }
+}
+
+initLoop()
