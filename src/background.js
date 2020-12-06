c297eb2a24ef12dde2f0a3e13e3f8590740a8dd2
Ville Touronen <ville.touronen@reaktor.com>
2020-12-06 14:04:50 +0200

    Add more page action icon sizes

--- src/background.js
+++ src/background.js
@@ -1,13 +1,17 @@
 const icons = {
     enabled: {
+        16: 'icons/icon-16.png',
         24: 'icons/icon-24.png',
+        32: 'icons/icon-32.png',
         48: 'icons/icon-48.png',
         64: 'icons/icon-64.png',
         128: 'icons/icon-128.png',
         256: 'icons/icon-256.png',
     },
     disabled: {
+        16: 'icons/icon-off-16.png',
         24: 'icons/icon-off-24.png',
+        32: 'icons/icon-off-32.png',
         48: 'icons/icon-off-48.png',
         64: 'icons/icon-off-64.png',
         128: 'icons/icon-off-128.png',
